84bc1199623ed4341ee6dea1718f8e63da4dd705
gyt <2877392542@qq.com>
2021-01-21 22:23:31 +0800

    第一次有点难

--- week1/src/Test.java
+++ week1/src/Test.java
@@ -1,11 +1,12 @@
-
 /**
  * @author Dale
  * @create 2021-01-21-21:44
  */
 public class Test {
 
-
+    public static void main(String[] args){
+        Test();
+    }
     public static void Test(){
         System.out.println("Hello");
     }
