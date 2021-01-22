1eab5316390f2b612443dc2cbe07389c0d089e36
gyt <2877392542@qq.com>
2021-01-22 12:01:51 +0800

    测试1

--- week1/src/Test1.java
+++ week1/src/Test1.java
@@ -41,4 +41,71 @@ public class Test1 {
 
     System.out.println(map.get("Dale"));
     }
+
+@Test
+    public void Test3(){
+        TreeSet<Student> treeSet = new TreeSet<>();
+
+        treeSet.add(new Student(1000,"Dale"));
+        treeSet.add(new Student(49,"Shally"));
+        treeSet.add(new Student(100,"HHH"));
+        treeSet.add(new Student(30,"A"));
+        treeSet.add(new Student(90,"HUAWEI"));
+        treeSet.add(new Student(10000,"GG"));
+
+        Iterator<Student> iterator = treeSet.iterator();
+        while (iterator.hasNext()){
+            Student student = iterator.next();
+
+            System.out.println(student);
+        }
+
+    }
+}
+
+class Student implements Comparable<Student>{
+
+    private String name;
+    private int score;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    public Student() {
+    }
+
+    public Student(int score, String name) {
+        this.score = score;
+        this.name = name;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+
+    @Override
+    public int compareTo(Student student) {
+        if (this.score>=student.score)
+            return 1;
+        else
+            return -1;
+    }
 }
