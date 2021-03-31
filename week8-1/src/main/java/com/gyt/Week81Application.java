093b526482e26e99d9c66b7b957c0023788a5ea0
gyt <2877392542@qq.com>
2021-03-31 23:40:31 +0800

    访问http://localhost:8080/all http://localhost:8080/transfer 来测试

---
+++ week8-1/src/main/java/com/gyt/Week81Application.java
@@ -0,0 +1,13 @@
+package com.gyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week81Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week81Application.class, args);
+    }
+
+}
---
+++ week8-1/src/test/java/com/gyt/Week81ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week81ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
